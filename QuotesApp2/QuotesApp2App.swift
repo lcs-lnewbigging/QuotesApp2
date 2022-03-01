f91f1f113f6bbfb95bc7b68abd644f3c78677048
Luke Newbigging <lnewbigging@lcs.on.ca>
2022-03-01 11:49:42 -0500

    Initial Commit

---
+++ QuotesApp2/QuotesApp2App.swift
@@ -0,0 +1,17 @@
+//
+//  QuotesApp2App.swift
+//  QuotesApp2
+//
+//  Created by Luke Newbigging on 2022-03-01.
+//
+
+import SwiftUI
+
+@main
+struct QuotesApp2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
